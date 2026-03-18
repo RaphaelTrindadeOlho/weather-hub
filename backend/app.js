fc63ea04a04b5dce3a37262072b696b8b7e38ad4
Raphael <raphaeltrindadeolho@gmail.com>
2026-03-17 21:22:18 -0300

    switch https to fetch with asycn/await in backend/js

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,37 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const https = require('https');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const API_KEY = process.env.API_KEY;
 
-app.post('/weather', (req, res) => {
+app.post('/api/weather', async (req, res) => {
   const city = req.body.city;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}&units=metric`;
 
-  https.get(url, (response) => {
-    let data = '';
-    response.on('data', (chunk) => {
-      data += chunk;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.cod !== 200) {
+      return res.status(data.cod || 500).json({ error: data.message || 'Erro ao obter dados do clima' });
+    }
+
+    const { main, weather } = data;
+
+    res.json({
+      temperature: main.temp,
+      description: weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${weather[0].icon}.png`
     });
-
-    response.on('end', () => {
-      const weatherData = JSON.parse(data);
-      const temperature = weatherData.main.temp;
-      const description = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
-
-      res.json ({
-        temperature: temperature,
-        description: description,
-        iconUrl: iconUrl
-      })
-
-    })
-
-  })
-
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter dados do clima' });
+  }
+});
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
